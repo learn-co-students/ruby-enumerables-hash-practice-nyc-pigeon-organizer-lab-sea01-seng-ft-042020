ec516ff5029677fd5aec2ee65ea9180fa0c4610b
John Rusch <johnrusch7@gmail.com>
2020-03-31 22:39:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons_by_name = {}
+  data.each do |attribute, groups| 
+    groups.each do |key, value|
+      value.each do |n|
+        unless pigeons_by_name.include?(n)
+          pigeons_by_name[n] = {}
+        end
+        unless pigeons_by_name[n].include?(attribute)
+          pigeons_by_name[n][attribute] = []
+        end
+        pigeons_by_name[n][attribute].push(key.to_s)
+      end
+    end
+  end
+  #binding.pry
+  pigeons_by_name
 end
