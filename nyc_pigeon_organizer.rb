add5ddd673e031ddd8306516e074788783566064
Mathew Wheatley <mathew.p.wheatley@gmail.com>
2020-04-02 19:48:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # Initializing pigeon_names
+  pigeon_names = []
+  # Loop through the two leveled hash to all included pigeon names
+  data.each_pair do |key_i, value_i|
+    value_i.each_pair do |key_j, value_j|
+      pigeon_names.push(value_j)
+    end
+  end
+  # Parse down to get only uniq pigeon_names
+  pigeon_names = pigeon_names.flatten.uniq
+  # Initializing new_data
+  new_data = {}
+  #  Populate new_data by matching pigeon_names with original data set
+  pigeon_names.collect do |name|
+    new_data[name] = {}
+    # Populating attributes keys
+    data.each_pair do |key_i, value_i|
+      new_data[name].merge!({key_i => []})
+      # Populating attributes values
+      value_i.each_pair do |key_j, value_j|
+        # Push attribute value if the name is found in original data set
+        new_data[name][key_i].push(key_j.to_s) if value_j.any? {|element| name == element}
+      end
+    end
+  end
+  new_data
 end
