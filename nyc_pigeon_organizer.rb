4f470b4aff5747e07941d2dfcdea081b68334449
Colton Kaiser <kaiserc2@seattleu.edu>
2020-04-03 20:43:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pidgeon_list = {}
+  data.each do |attribute, value|
+    value.each do |value2, names|
+      names.each do |name|
+          if pidgeon_list[name] == nil
+            pidgeon_list[name] = {}
+          end
+          if pidgeon_list[name][attribute] == nil
+            pidgeon_list[name][attribute] = []
+          end
+          pidgeon_list[name][attribute] << value2.to_s
+ #                   binding.pry
+      end
+    end
+  end
+    pidgeon_list
 end
+
+# #pigeon_list = {
+# #  "Theo" => {
+# #    :color => ["purple", "grey"],
+# #    :gender => ["male"],
+#     :lives => ["Subway"]
+#   },
